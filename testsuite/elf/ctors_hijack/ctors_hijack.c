753f0f74b30052696408c1577e31a7ff9b12840a
duanrock <duanrock@foxmail.com>
2020-11-22 05:46:07 -0800

    modify ctors_hijack.c

--- testsuite/elf/ctors_hijack/ctors_hijack.c
+++ testsuite/elf/ctors_hijack/ctors_hijack.c
@@ -18,11 +18,14 @@ int		main(int argc, char **argv)
 {
   elfshobj_t	*file;
   elfsh_Sym	*new_ctors;
+  elfsh_Sym	*my_ctors;
   int		ret;
 
 
   file = elfsh_map_obj(TROJANED_FILE);
+  printf("before elfsh_get_symbol_by_name,new_ctors\n");
   new_ctors = elfsh_get_symbol_by_name(file, "new_ctors");
+  my_ctors = elfsh_get_symbol_by_name(file, "my_ctors");
   if (!file || !new_ctors)
     {
       elfsh_error();
@@ -35,7 +38,15 @@ int		main(int argc, char **argv)
   /***********************************************************/
 
   //ret = elfsh_set_ctors_entry_by_index(file, 1, new_ctors->st_value);
+  printf("###before new_ctors->st_value=%lu,my_ctors->st_value=%lu\n",new_ctors->st_value,my_ctors->st_value);
   ret = elfsh_set_ctors_entry_by_name(file, "my_ctors", new_ctors->st_value);
+//  printf("####change new_ctors,to %d\n",my_ctors->st_value);
+//  ret = elfsh_set_ctors_entry_by_name(file, "new_ctors",4448);
+
+  new_ctors = elfsh_get_symbol_by_name(file, "new_ctors");
+  my_ctors = elfsh_get_symbol_by_name(file, "my_ctors");
+  
+  printf("###after new_ctors->st_value=%lu,my_ctors->st_value=%lu\n",new_ctors->st_value,my_ctors->st_value);
   if (ret < 0)
     {
       elfsh_error();
